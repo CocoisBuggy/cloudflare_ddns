da32a740748f47b803f83e0ee1c07e37b0a9c9cc
CocoisBuggy <g.colinharley@gmail.com>
2025-02-04 12:49:41 +0200

    WIP: Allow all domain option to get passed as file name

--- flake.nix
+++ flake.nix
@@ -102,43 +102,73 @@
                 '';
               };
 
+              domain_name = lib.mkOption {
+                type = lib.types.str;
+                example = "example.com";
+                decription = "If this is an A record, you can specify a domain name or sub domain name";
+              };
+
               api_key_file = lib.mkOption {
                 types = lib.types.str;
                 description = "For security, I like to pass this in as a file that contains the keyfile (sops, for example)";
                 example = "/run/secrets/keys/cloudflare";
               };
 
-              domain_name = lib.mkOption {
+              zone_id_file = lib.mkOption {
+                type = lib.types.str;
+                description = config.services.coco-ddns.zone_id;
+              };
+
+              record_file = lib.mkOption {
+                types = lib.types.str;
+                description = ''
+                  Like zones in cloudflare, records have arbitrarily assigned IDs that you can
+                  use to address a record. This can be useful because if you edit this record in
+                  the cloudflare dashboard, even in drastic ways, this script will be able to
+                  find and update it in perpetuity. If you have this value, you should use it.
+                '';
+              };
+
+              domain_name_file = lib.mkOption {
                 type = lib.types.str;
                 example = "example.com";
                 decription = "If this is an A record, you can specify a domain name or sub domain name";
               };
             };
 
-            config = lib.mkIf config.services.coco-ddns.enable {
-              systemd.services.coco-ddns = {
-                description = "Dynamic DNS updater";
-                serviceConfig = {
-                  Type = "oneshot";
-                  ExecStart = ''
-                    ${self.packages."${system}".default}/bin/coco-ddns \
-                      --zone_id ${config.services.coco-ddns.zone_id} \
-                      --record ${config.services.coco-ddns.record} \
-                      --api_key $(cat ${config.services.coco-ddns.api_key_file}) \
-                      --domain_name ${config.services.coco-ddns.domain_name}
-                  '';
-                  Restart = "no";
+            config =
+              let
+                cfg = config.services.coco-dns;
+                readFile = file_name: "$(cat ${file_name})";
+                pass =
+                  file_name: val: "$(if [[ -f ${file_name} ]]; then ${readFile file_name} else echo ${val} fi)";
+              in
+              lib.mkIf config.services.coco-ddns.enable {
+                systemd.services.coco-ddns = {
+                  description = "Dynamic DNS updater";
+                  serviceConfig = {
+                    Type = "oneshot";
+                    # At runtime, read the specified keyfile path and, if it is present,
+                    # use it over the provided literal value.
+                    ExecStart = ''
+                      ${self.packages."${system}".default}/bin/coco-ddns \
+                        --zone_id     ${pass cfg.zone_file cfg.zone_id} \
+                        --record      ${pass cfg.record_file cfg.record} \
+                        --domain_name ${pass cfg.domain_name_file cfg.domain_name} \
+                        --api_key     ${readFile cfg.api_key_file}
+                    '';
+                    Restart = "no";
+                  };
                 };
-              };
 
-              systemd.timers.coco-ddns = {
-                wantedBy = [ "timers.target" ];
-                timerConfig = {
-                  OnCalendar = config.services.coco-ddns.interval;
-                  Persistent = true;
+                systemd.timers.coco-ddns = {
+                  wantedBy = [ "timers.target" ];
+                  timerConfig = {
+                    OnCalendar = config.services.coco-ddns.interval;
+                    Persistent = true;
+                  };
                 };
               };
-            };
           };
       }
     );
